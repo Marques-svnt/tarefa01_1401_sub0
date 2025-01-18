5e315583118edbebcda315404f5960471d6da414
PauloCesar53 <pcdilauro53@gmail.com>
2025-01-18 20:51:27 -0300

    Função Para ligar led azul e desligar os demais

--- main.c
+++ main.c
@@ -1,14 +1,24 @@
-#include <stdio.h>
 #include "pico/stdlib.h"
 
+#define LED_B_PIN 12
 
+
+//Função para ligar led azul e desiga os demais 
+void Lig_Led_B(){
+    gpio_put(LED_B_PIN,1);
+    gpio_put(11,0);//desliga led verde 
+    gpio_put(13,0);//desliga led vermelhho 
+}
 
 int main()
 {
-    stdio_init_all();
+    //configuração inicial Leds
+    gpio_init(LED_B_PIN);//inicia o pino 
+    gpio_set_dir(LED_B_PIN, GPIO_OUT);//define como saída 
 
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+    while(true){
+        
     }
+    return 0;
 }
+
