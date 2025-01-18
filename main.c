d40423c643bca10d1d961d2f9dba15833c2ceeb0
PauloCesar53 <pcdilauro53@gmail.com>
2025-01-18 20:05:33 -0300

    Função Ligar e desligar Led Azul

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+//configurar  leds
+gpio_init(LED_B_PIN);
+gpio_set_dir(LED_B_PIN, GPIO_OUT);
 
+void Lig_Led_Azul(){
+    gpio_put(LED_B_PIN,1);
+}
+//função para desligar Led Azul 
+void Des_Led_Azul(){
+    gpio_put(LED_B_PIN,0);
+}
 
 int main()
 {
