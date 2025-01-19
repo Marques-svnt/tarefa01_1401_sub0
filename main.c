75a15c89c7809fce90b5c701a239285fd847d4bc
ribeiro51 <lucasribeiro.ee@gmail.com>
2025-01-18 21:18:16 -0300

    Código para acender o LED verde e apagar os demais

--- main.c
+++ main.c
@@ -1,14 +1,22 @@
-#include <stdio.h>
 #include "pico/stdlib.h"
 
+#define LED_GREEN 11    // led verde ligado na porta 11
 
+//Função para ligar led verde e desligar os demais 
+void Lig_Led_Green(){
+    gpio_put(LED_GREEN,1); // liga o led verde
+    gpio_put(12,0);//desliga led azul
+    gpio_put(13,0);//desliga led vermelho 
+}
 
 int main()
 {
-    stdio_init_all();
+    //configuração inicial Leds
+    gpio_init(LED_GREEN);//inicia o pino 
+    gpio_set_dir(LED_GREEN, GPIO_OUT);//define como saída 
 
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+    while(true){
+        
     }
+    return 0;
 }
